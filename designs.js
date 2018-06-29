6af7951009850176e09128720a1f96d3c69f2d62
always42nate <40600820+always42nate@users.noreply.github.com>
2018-06-29 19:26:20 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,31 @@
 
 // When size is submitted by the user, call makeGrid()
 
+var rowsInput;
+var colsInput;
+
+//function building the grids
 function makeGrid() {
-
-// Your code goes here!
-
+    var table = document.getElementById("pixelCanvas");
+//resets table ,when the submit button is clicked 
+    while(table.rows.length > 0)
+        table.deleteRow(0);
+    colsInput = document.getElementById("inputWeight").value;
+    rowsInput = document.getElementById("inputHeight").value;
+    for (var i = 0; i < rowsInput; i++) {
+        var row_elem = table.insertRow(i);
+        row_elem.setAttribute("class", "row");
+        for (var j = 0; j < colsInput; j++) {
+            var cell= row_elem.insertCell(j);
+            cell.addEventListener('click', function(evt) {
+                evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
+            });
+        }
+    }
+    return false;
 }
+
+document.getElementById("sizePicker").addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    makeGrid();
+});
